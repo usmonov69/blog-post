87d338dc69baa2cb5d94c2ae1e2c55c871d9d4e2
usmonov69 <usmonovraxmat600@gmail.com>
2021-12-07 15:48:03 +0000

    updated file

--- blog/settings.py
+++ blog/settings.py
@@ -163,15 +163,15 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+if DEBUG:
+    STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+else:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
 
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
